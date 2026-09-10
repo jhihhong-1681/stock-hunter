3dca2c4141e309887aa2e69ec8984fd7e3b57083
jhihhong-1681 <jhihhong0810@gmail.com>
2026-09-10 11:21:41 +0800

    每日快照 2026-09-09

--- portfolio-calendar/data.js
+++ portfolio-calendar/data.js
@@ -94,5 +94,6 @@ window.PORTFOLIO_HISTORY = [
   { date: "2026-09-03", total: 1202450, basis: "total_assets" },
   { date: "2026-09-04", total: 1163148, basis: "total_assets" },
   { date: "2026-09-07", total: 1163148, basis: "total_assets" },
-  { date: "2026-09-08", total: 1192613, basis: "total_assets" }
+  { date: "2026-09-08", total: 1192613, basis: "total_assets" },
+  { date: "2026-09-09", total: 1196240, basis: "total_assets" }
 ];
